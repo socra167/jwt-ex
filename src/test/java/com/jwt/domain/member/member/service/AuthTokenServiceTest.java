3b8ac96c3c773e61ae8d41697ff60baf54b405e3
socra167 <socra167@gmail.com>
2025-02-07 15:10:41 +0900

    test: parsing된 JWT 검증 로직 추가

--- src/test/java/com/jwt/domain/member/member/service/AuthTokenServiceTest.java
+++ src/test/java/com/jwt/domain/member/member/service/AuthTokenServiceTest.java
@@ -2,8 +2,9 @@ package com.jwt.domain.member.member.service;
 
 import static org.assertj.core.api.Assertions.*;
 
-import java.security.Key;
 import java.util.Map;
+
+import javax.crypto.SecretKey;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -15,6 +16,8 @@ import org.springframework.transaction.annotation.Transactional;
 import com.jwt.domain.member.member.entity.Member;
 import com.jwt.standard.Ut;
 
+import io.jsonwebtoken.Jwt;
+import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 
 @SpringBootTest
@@ -39,14 +42,25 @@ public class AuthTokenServiceTest {
 		int expireSeconds = 60 * 60 * 24 * 365;
 
 		// 토큰 시크릿 키
-		Key secretKey = Keys.hmacShaKeyFor("abcdefghijklmnopqrstuvwxyz1234567890abcdefghijklmnopqrstuvwxyz1234567890".getBytes());
+		SecretKey secretKey = Keys.hmacShaKeyFor(
+			"abcdefghijklmnopqrstuvwxyz1234567890abcdefghijklmnopqrstuvwxyz1234567890".getBytes());
 
-		String jwt = Ut.Jwt.createToken(secretKey, expireSeconds, Map.of("name", "john", "age", 23 ));
+		Map<String, Object> originPayload = Map.of("name", "john", "age", 23);
+		String jwtStr = Ut.Jwt.createToken(secretKey, expireSeconds, originPayload);
+		assertThat(jwtStr).isNotBlank();
 
-		assertThat(jwt).isNotBlank();
-		System.out.println("jwt = " + jwt);
+		// 파싱하는 과정에서, JWT가 누구에게나 공개되어 있어 위변조가 있을 수 있다. 또 유효기간이 지나면 JWT는 동작하면 안된다.
+		// -> parse() 과정을 살펴보자 - ExpiredJwtException, MalformedJwtException, SignatureException, SecurityException, IllegalArgumentException ...
+		// 메서드 안에 이미 구현되어 있다. 잘못된 JWT가 들어오면 이미 데이터를 꺼내오기 전에 예외가 발생한다
+		Jwt<?, ?> parsedJwt = Jwts
+			.parser()
+			.verifyWith(secretKey)
+			.build()
+			.parse(jwtStr);
+
+		Map<String, Object> parsedPayload = (Map<String, Object>)parsedJwt.getPayload(); // payload는 Claims 형태로 저장했다
+		assertThat(parsedPayload).containsAllEntriesOf(originPayload); // issuedAt, expiration가 추가되어서 완전히 일치하진 않는다
 	}
-
 
 	@Test
 	@DisplayName("Access Token 생성")
