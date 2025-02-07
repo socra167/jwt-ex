e3214475131f088bf5928d2d1515284ab29f735b
socra167 <socra167@gmail.com>
2025-02-07 15:43:54 +0900

    test: accessToken에서 가져온 payload 검증 테스트 실패 / `id`가 `long`이 아닌 문제

--- src/test/java/com/jwt/domain/member/member/service/AuthTokenServiceTest.java
+++ src/test/java/com/jwt/domain/member/member/service/AuthTokenServiceTest.java
@@ -72,5 +72,11 @@ public class AuthTokenServiceTest {
 		String accessToken = authTokenService.genAccessToken(member);
 		boolean isValid = Ut.Jwt.isValidToken(secretKey, accessToken);
 		assertThat(isValid).isTrue();
+
+		Map<String, Object> parsedPayload = Ut.Jwt.getPayload(secretKey, accessToken);
+
+		assertThat(parsedPayload).containsAllEntriesOf(
+			Map.of("id", member.getId(), "username", member.getUsername())
+		);
 	}
 }
